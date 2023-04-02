a15e14f20736905ed461369692328830048c75ad
Vladimir <sviderskijvladimir@gmail.com>
2023-04-02 23:13:16 +0300

    show the spinner in aside bar

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ function App() {
         lg: `"nav nav" "aside main"`
       }}
       templateColumns={{
-        base: '1fr',
-        lg: '200px 1fr'
+        base: "1fr",
+        lg: "200px 1fr"
       }}
     >
       <GridItem area={"nav"}>
         <NavBar />
       </GridItem>
-      <Show above="lg">
+      <Show>
         <GridItem area={"aside"} paddingX={5}>
           <GenreList />
         </GridItem>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -20,7 +20,6 @@ const GameGrid = () => {
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
-              {" "}
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
