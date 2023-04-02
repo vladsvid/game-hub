a5b44514cf09d29e82ea5d7e560b5e3b202a0217
Vladimir <sviderskijvladimir@gmail.com>
2023-04-03 02:35:32 +0300

    Handle games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,8 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage;
+
   const target = "media/";
   const crop = "crop/600/400/";
   const index = url.indexOf(target) + target.length;
