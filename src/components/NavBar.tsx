243ffc3123827b08199458447ea8525a6bd9200a
Vladimir <sviderskijvladimir@gmail.com>
2023-04-03 11:32:30 +0300

    Implemet searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,14 @@ import logo from "../assets/logo.webp";
 import ColorModeToggle from "./ColorModeToggle";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => (
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => (
   <HStack padding="10px">
     <Image src={logo} boxSize="60px" />
-    <SearchInput />
+    <SearchInput onSearch={onSearch} />
     <ColorModeToggle />
   </HStack>
 );
